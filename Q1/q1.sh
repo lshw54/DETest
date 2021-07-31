39504b80069196cb2b5ae398497b2e7b3bd32d33
EC2 Default User <ec2-user@ip-172-31-56-196.ec2.internal>
2021-07-31 03:12:53 +0000

    Add Q2-1

--- Q1/q1.sh
+++ Q1/q1.sh
@@ -6,5 +6,5 @@ gzip -d NASA_access_log_Aug95.gz
 
 wc -l NASA_access_log_Aug95
 
-egrep '18/Aug|19/Aug|20/Aug' NASA_access_log_Aug95 | cut -d '' -f 1 | sort | uniq -c | sort -r | head -10
+egrep '18/Aug|19/Aug|20/Aug' NASA_access_log_Aug95 | cut -d '' -f 1 | sort | uniq -c | sort -r -n | head -10
 
