aae33008786ba20c126d35aa1955b40d1f6104d7
EC2 Default User <ec2-user@ip-172-31-91-26.ec2.internal>
2021-08-03 07:41:29 +0000

    Add Result Chart

--- Q3/app/main/routes.py
+++ Q3/app/main/routes.py
@@ -18,13 +18,14 @@ def home():
         Vote.id += 1
         Vote.vote_id += 1
         flash('Thanks for Vote!', 'success')
+        return redirect(url_for('main.result'))
     return render_template('index.html', title="Index", form=form)
 
 @main.route("/result", methods=['GET'])
 def result():
-    result = Vote.query.order_by(Vote.candidates).all()
+    #result = Vote.query.order_by(Vote.candidates).all()
     c1 = Vote.query.order_by(Vote.candidates.endswith('薯片')).count()
     c2 = Vote.query.filter(Vote.candidates.endswith('林林')).count()
     c3 = Vote.query.filter(Vote.candidates.endswith('正氣')).count()
-    return render_template('result.html', title='Vote Result', result=result, c1=c1, c2=c2, c3=c3)
+    return render_template('result.html', title='Vote Result', c1=c1, c2=c2, c3=c3)
     
