4daa14d6fefdc9fb4cd9962d9df5130dec5c5af5
EC2 Default User <ec2-user@ip-172-31-91-26.ec2.internal>
2021-08-03 03:25:46 +0000

    Finish Vote Result

--- Q3/app/main/forms.py
+++ Q3/app/main/forms.py
@@ -2,6 +2,4 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField
 
 class VoteForm(FlaskForm):
-    vote1 = SubmitField(label='Support')
-    vote2 = SubmitField('Support', render_kw={"class": "btn btn-success", "id": "btn-sub"})
-    vote3 = SubmitField('Support', render_kw={"class": "btn btn-success", "id": "btn-sub"})
+    vote = SubmitField('Support')
--- Q3/app/main/routes.py
+++ Q3/app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, flash, redirect, url_for
+from flask import render_template, Blueprint, flash, redirect, url_for, request
 from app.models import Vote
 from app.main.forms import VoteForm
 from app import db
@@ -6,26 +6,25 @@ import datetime
 
 main = Blueprint('main', __name__)
 
-@main.route("/")
+@main.route("/", methods=['GET', 'POST'])
 @main.route("/index", methods=['GET', 'POST'])
 def home():
     form = VoteForm()
-    #if form.validate_on_submit():
-    c = '薯片'
-    vote_btn = Vote(candidates=c)
-    db.session.add(vote_btn)
-    db.session.commit()
-    Vote.id += 1
-    Vote.vote_id += 1
-    flash('Thanks for Vote!', 'success')
-        #return redirect(url_for('main.result'))
+    if form.validate_on_submit():
+        c='林林'
+        vote1_btn = Vote(candidates=c)
+        db.session.add(vote1_btn)
+        db.session.commit()
+        Vote.id += 1
+        Vote.vote_id += 1
+        flash('Thanks for Vote!', 'success')
     return render_template('index.html', title="Index", form=form)
-
 
 @main.route("/result", methods=['GET'])
 def result():
     result = Vote.query.order_by(Vote.candidates).all()
-    #tables=[df.to_html(classes='data', header="true")]
-    print(result)
-    return render_template('result.html', title='Vote Result', result=result)
+    c1 = Vote.query.order_by(Vote.candidates.endswith('薯片')).count()
+    c2 = Vote.query.filter(Vote.candidates.endswith('林林')).count()
+    c3 = Vote.query.filter(Vote.candidates.endswith('正氣')).count()
+    return render_template('result.html', title='Vote Result', result=result, c1=c1, c2=c2, c3=c3)
     
